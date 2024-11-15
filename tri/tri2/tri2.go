9a592c987884d2eb59aacd48eabbeb06b94856bd
Kéwan B <mail@kewan.fr>
2024-11-15 16:14:46 +0100

    🎉 tri/tri2 Résolu - Difficulté 2/5 (5 min)

--- tri/tri2/tri2.go
+++ tri/tri2/tri2.go
@@ -17,5 +17,25 @@ le tableau donné en entrée.
 */
 
 func triabs(tinit []int) (tfin []int) {
+	tfin = append(tfin, tinit...)
+
+	var i, j int
+	for i = 1; i < len(tfin); i ++ {
+		v := tfin[i]
+		for j = i; j > 0 && abs(tfin[j - 1]) < abs(v); j-- {
+			tfin[j] = tfin[j-1]
+		}
+		tfin[j] = v
+
+	}
+
+
 	return tfin
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return (-1)*x
+	}
+	return x
 }
